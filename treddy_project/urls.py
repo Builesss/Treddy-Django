76f5a8bf144a99159642ac3ce89051af1a078224
Sebastian Builes <sebastian.builes@treddy.com>
2026-06-22 18:00:11 -0500

    fix: media URL serving para imagenes, reemplazar boton placeholder por badge 'proximamente'

--- treddy_project/urls.py
+++ treddy_project/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,7 @@ urlpatterns = [
     path('productos/', include('productos.urls')),
     path('', RedirectView.as_view(url='usuarios/login/', permanent=False)),
 ]
+
+# Servir archivos de media en desarrollo (DEBUG=True)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
